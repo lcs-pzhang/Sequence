439a1f6a45acf8165cc11eb3eb4ea1a0eb61508e
Zhang Phillip <pzhang@lcs-mba3156.lcs.on.ca>
2017-09-30 11:21:39 -0400

    finished the sun rays, got the cloud and got the red for the rainbow.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,26 +21,59 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 600, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+// Makes all shapes drawn after this command be filled blue
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
 
-// Show where the origin is
-canvas.drawAxes()
+// Draw a rectangle
+canvas.drawRectangle(centreX: 300, centreY: 250, width: 600, height: 500)
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+canvas.fillColor=Color.yellow
+canvas.borderColor=Color.yellow
+//Draw a circle
+canvas.drawEllipse(centreX: 0, centreY: 500, width: 150, height: 150)
 
-// Draw a line on top of the elipse
-canvas.drawLine(fromX: 160, fromY: 290, toX: 175, toY: 400)
+canvas.lineColor=Color.yellow
+//Draw a line
+canvas.defaultLineWidth=5
+canvas.drawLine(fromX: 70, fromY: 470, toX: 600, toY: 400)
 
 //Draw a line
-canvas.drawLine(fromX: 175, fromY: 400, toX: 170, toY: 430)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
 
 //Draw a line
-canvas.drawLine(fromX: 170, fromY: 430, toX: 200, toY: 500)
+canvas.drawLine(fromX: 50, fromY: 450, toX: 300, toY: 0)
+
+canvas.lineColor=Color.orange
+//Draw a line
+canvas.drawLine(fromX: 40, fromY: 435, toX: 200, toY: 0)
+
+//Draw a line
+canvas.drawLine(fromX: 65, fromY: 460, toX: 500, toY: 0)
+
+canvas.borderColor=Color.white
+canvas.fillColor=Color.white
+//Draw a circle
+canvas.drawEllipse(centreX: 600, centreY: 500, width: 150, height: 150)
+
+//Draw a circle
+canvas.drawEllipse(centreX: 500, centreY: 500, width: 150, height: 150)
+
+//Draw a circle
+canvas.drawEllipse(centreX: 600, centreY: 400, width: 150, height: 150)
+
+canvas.fillColor=Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+canvas.borderColor=Color.red
+canvas.defaultBorderWidth = 30
+//Draw a circle
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 600, height: 600)
+
+canvas.fillColor=Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+
+
+
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
